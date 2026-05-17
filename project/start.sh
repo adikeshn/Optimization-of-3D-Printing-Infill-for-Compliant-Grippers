7452e9d20a3e379b9cb58adaee06876300662f01
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2026-05-16 23:52:51 -0400

    Update start.sh

--- project/start.sh
+++ project/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-celery -A celery_app worker --loglevel=info &
+celery -A celery_app worker --loglevel=info --concurrency=1 --max-tasks-per-child=1 &
 
-exec gunicorn app:app --bind 0.0.0.0:10000 --timeout 120
+exec gunicorn app:app --bind 0.0.0.0:10000 --timeout 120 --workers 1
