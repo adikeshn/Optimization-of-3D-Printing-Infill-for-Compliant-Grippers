a204d1bca853f84d19cb2cc88b31906e4afff917
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2026-05-16 23:43:32 -0400

    Update celery_app.py

--- project/celery_app.py
+++ project/celery_app.py
@@ -5,4 +5,5 @@ celery = Celery(
     "infill_jobs",
     broker=os.environ.get("REDIS_URL", "redis://localhost:6379/0"),
     backend=os.environ.get("REDIS_URL", "redis://localhost:6379/0"),
+    include=["tasks"],
 )
